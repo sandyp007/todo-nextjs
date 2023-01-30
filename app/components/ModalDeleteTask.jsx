6f5332e9b55616a7d48476d300f4052daee9f910
Raul-TC <raultc.9819@gmail.com>
2023-01-29 21:59:59 -0600

    fix bug in modal delete

--- app/components/ModalDeleteTask.jsx
+++ app/components/ModalDeleteTask.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import taskContext from '../context/taskContext'
 
 const ModalDeleteTask = ({ setModalTask, id, type, setModal }) => {
-    const { dbTasks, handleDeleteTask, handleShowTemporalModal } = useContext(taskContext)
+    const { dbTasks, handleDeleteTask } = useContext(taskContext)
 
     const lenghtCompleted = dbTasks.filter(el => el.isDone === true)
 
@@ -11,7 +11,7 @@ const ModalDeleteTask = ({ setModalTask, id, type, setModal }) => {
             <div className='dark:bg-containerDark dark:text-textDark bg-containerLight text-textLight absolute rounded-2xl flex flex-col items-center justify-center p-7 w-[95%] md:max-w-md'>
                 <h1 className='text-3xl font-bold'>{type === 'one' ? 'Delete Task' : `Delete ${lenghtCompleted.length} Task`}</h1>
                 <p>
-                    {type === 'one' ? "Are you sure you want to delete this take? This will delete the task and cannot be undone." : `Are you sure you want to delete ${lengthCompleted.length} completed tasks? This will delete the tasks and cannot be undone.`}
+                    {type === 'one' ? "Are you sure you want to delete this take? This will delete the task and cannot be undone." : `Are you sure you want to delete ${lenghtCompleted.length} completed tasks? This will delete the tasks and cannot be undone.`}
                 </p>
                 <div className='flex justify-between w-full gap-3 mt-3'>
                     <button className='text-base font-bold p-3 rounded-md cursor-pointer w-2/3 bg-gray-600 text-white' onClick={() => !id ? setModal(false) : setModalTask(false)}>
